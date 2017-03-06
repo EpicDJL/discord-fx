0a3b8d627b3213bbabe3d0fb293255eefe8344e3
EpicDJL <awesomeriptide@gmail.com>
2017-03-06 09:35:17 +0530

    added more

---
+++ setup laugh.bat
@@ -0,0 +1,8 @@
+sh fx_encode.sh Short_laugh.wav ah ah
+sh fx_encode.sh Short_laugh_Wah.wav ah_w ah_w
+sh fx_encode.sh Short_laugh_Reverse.wav ah_r ah_r
+sh fx_encode.sh Short_laugh_Stretch.wav ah_s ah_s
+sh fx_encode.sh Long_Laugh.wav ahh ahh
+sh fx_encode.sh Long_Laugh_Wah.wav ahh_w ahh_w
+sh fx_encode.sh Long_Laugh_Reverse.wav ahh_r ahh_r
+sh fx_encode.sh Long_Laugh_Stretch.wav ahh_s ahh_s
