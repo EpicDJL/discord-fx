4a281a360256525334827f764116596945c6f744
Declan Li-Carney <awesomeriptide@gmail.com>
2017-03-06 19:28:22 +0530

    stuff

---
+++ config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const CONFIG = {
+    discord: {
+        token: "Mjg2NzM0MjAzNDkwNjY0NDQ4.C51S4g.56nQxQ9jubt8OW4o134V_G62Y2s"
+    },
+    prefix: "!"
+};
+
+module.exports = CONFIG;
